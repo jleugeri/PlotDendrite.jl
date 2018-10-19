56838dfd1fd20afade4df0bd6e47f6e703ff09c8
jleugeri <jleugeri@uos.de>
2018-10-19 21:32:42 +0200

    changed interface to used named tuple

--- src/PlotDendrite.jl
+++ src/PlotDendrite.jl
@@ -4,24 +4,24 @@ export plotDendrite
 using Compose
 const offset = 1 # how many synapse widths correspond to the connector width
 
-function _draw_branch(name, leaf_colors, relative_thickness, branch_color, sub_branches, args...; fill_connectors=false, connector_style= fill_connectors ? [] : [stroke("black"), linewidth(1)], style=[stroke("black")], h_frac=0.5, font_style=[stroke("black"), fill("white"), fontsize(12), linewidth(0.1)], kwargs...)
+function _draw_branch(root; fill_connectors=false, connector_style= fill_connectors ? [] : [stroke("black"), linewidth(1)], style=[stroke("black")], h_frac=0.5, font_style=[stroke("black"), fill("white"), fontsize(12), linewidth(0.1)], kwargs...)
     branch_drawings = []
     branch_root_loc = Tuple{Float64,Float64}[]
     branch_total_lengths = Int64[]
     branch_total_branches = Int64[]
-    for branch ∈ sub_branches
-        branch_drawing, branch_total_length, branch_total_branches_, by0, by1 = _draw_branch(branch..., args...; fill_connectors=fill_connectors, connector_style=connector_style, style=style, h_frac=h_frac, font_style=font_style, kwargs...)
+    for branch ∈ root.branches
+        branch_drawing, branch_total_length, branch_total_branches_, by0, by1 = _draw_branch(branch; fill_connectors=fill_connectors, connector_style=connector_style, style=style, h_frac=h_frac, font_style=font_style, kwargs...)
         push!(branch_drawings, branch_drawing)
         push!(branch_root_loc, (by0, by1))
         push!(branch_total_lengths, branch_total_length)
         push!(branch_total_branches, branch_total_branches_)
     end
     total_branches = isempty(branch_total_branches) ? 1 : sum(branch_total_branches)
-    branch_length = length(leaf_colors) + offset
+    branch_length = length(root.SC) + offset
     branches_total_length = isempty(branch_total_lengths) ? 0 : maximum(branch_total_lengths)
     total_length = branches_total_length + branch_length
     width  = branch_length/total_length
-    height  = (1/total_branches)*h_frac*relative_thickness#-1.5*1/(aspect)/total_length)*relative_thickness
+    height  = (1/total_branches)*h_frac*root.DW
     x0 = 1-width
     y0 = (1-height)*0.5 + 0.5/total_length#0.5-0.5*height# + 0.75*1/(aspect)/total_length
     lw = 1/total_length
@@ -32,12 +32,12 @@ function _draw_branch(name, leaf_colors, relative_thickness, branch_color, sub_b
     drawing = (
             context(0.01,0.01, 0.98, 0.98),
             # draw stem
-            (context(x0,0,width,1), fill(branch_color),
+            (context(x0,0,width,1), fill(root.DC),
                 (context(offset/branch_length, y0, 1-offset/branch_length, height), 
                     # draw rectangle representing the stem
                     rectangle(),
                     # write name
-                    (context(), text(0.5, 0.5, name, hcenter, vcenter), font_style...)
+                    (context(), text(0.5, 0.5, root.name, hcenter, vcenter), font_style...)
                 ),
                 # draw connectors
                 if fill_connectors # filled connectors in parent context
@@ -46,11 +46,11 @@ function _draw_branch(name, leaf_colors, relative_thickness, branch_color, sub_b
                     ]; connector_style...]
                 else # bezier connectors in their own context
                     [(context(), [
-                        curve((0,branch_ys[i]+0.5*(by0+by1)*branch_heights[i]), (0.7*offset/branch_length,branch_ys[i]+0.5*(by0+by1)*branch_heights[i]), (0.3*offset/branch_length,y0+height*i/(length(sub_branches)+1)), (offset/branch_length,y0+height*i/(length(sub_branches)+1))) for (i,(by0,by1)) ∈ enumerate(branch_root_loc)
+                        curve((0,branch_ys[i]+0.5*(by0+by1)*branch_heights[i]), (0.7*offset/branch_length,branch_ys[i]+0.5*(by0+by1)*branch_heights[i]), (0.3*offset/branch_length,y0+height*i/(length(root.branches)+1)), (offset/branch_length,y0+height*i/(length(root.branches)+1))) for (i,(by0,by1)) ∈ enumerate(branch_root_loc)
                     ]..., connector_style...)]
                 end...,
                 
-                if isempty(sub_branches)
+                if isempty(root.branches)
                     # draw end-cap
                     [circle(offset/total_length, y0+0.5*height, 0.5h*height)]
                 else
@@ -67,7 +67,7 @@ function _draw_branch(name, leaf_colors, relative_thickness, branch_color, sub_b
             
             # draw synapses
             [
-                (context(), arc(x0+(offset+i-1+0.5)*lw, y0, 0.4*lw, π, 2π), fill(lc)) for (i,lc) ∈ enumerate(leaf_colors)
+                (context(), arc(x0+(offset+i-1+0.5)*lw, y0, 0.4*lw, π, 2π), fill(lc)) for (i,lc) ∈ enumerate(root.SC)
             ]...,
             
             
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,16 @@
 using PlotDendrite, Compose, Colors, Test
 
-structure = ("A", [RGB(0.3, 0.3, 0.3),RGB(0.3, 0.7, 0.3),"black","pink"], 1.0, "red", [("B1", ["black", "black"], 1.0, "blue", []),("B2", ["black", "black"], 0.75, "blue", []),("B3", ["black", "black"], 0.5, "yellow", [("C1", ["orange"], 1.0, "brown",[]),("C2", ["orange"], 1.0, "brown", [])])])
+structure = (name="A", SC=[RGB(0.3, 0.3, 0.3),RGB(0.3, 0.7, 0.3),"black","pink"], DW=1.0, DC="red", branches=[
+    (name="B1", SC=["black", "black"], DW=1.0, DC="blue", branches=[]),
+    (name="B2", SC=["black", "black"], DW=0.75, DC="blue", branches=[]),
+    (name="B3", SC=["black", "black"], DW=0.5, DC="yellow", branches=[
+        (name="C1", SC=["orange"], DW=1.0, DC="brown",branches=[]),
+        (name="C2", SC=["orange"], DW=1.0, DC="brown", branches=[])
+    ])
+])
+
 kwargs = (h_frac=0.5, style=[])
-res = plotDendrite(structure..., canvas=SVG("test.svg", 15cm,10cm))
+res = plotDendrite(structure, canvas=SVG("test.svg", 15cm,10cm))
 
 @testset "plotting" begin
     @test res.height == 4
